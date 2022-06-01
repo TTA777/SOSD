991a1bb71815d6661914671e3ed0fa19a50eb3af
Thomas Andersen <d6di6skcprs5@opayq.com>
2022-06-01 13:18:34 +0200

    Update script for parsing csv to latex plots

--- scripts/csv_to_latex.py
+++ scripts/csv_to_latex.py
@@ -13,7 +13,7 @@ def read_file(file_name) -> list[list[str]]:
     split_lines = []
     # Strips the newline character
     for line in lines:
-        split_lines.append(line.split("\t"))
+        split_lines.append(line.split(","))
     return split_lines
 
 
@@ -44,14 +44,60 @@ def parse_lines(lines: list[list[str]], columns: list[int], pattern: str):
                 val += float((line[column]))
             if val > highest_number:
                 highest_number = val
-    # print(graph_data)
-    # print(highest_number)
+            # print(graph_data)
+    print(graph_data)
+    print(highest_number)
     if len(columns) == 1:
         desc = headers[columns[0]].replace('_', ' ')
     else:
         desc = headers[0]  # Stacked graphs have lookup_time as y label
 
+    print(workload + " " + pattern + " " + desc)
+    print()
+    print()
+    print()
+    print()
     return graph_data, highest_number, desc
+
+
+def make_mixed_plots(lines: list[list[str]], columns: list[int]):
+    patterns = [r'(osm)',
+                r'(books)']
+
+    highest_num = 0
+    for pattern in patterns:
+        _, max_val, _ = parse_lines(lines, columns, pattern)
+        if max_val > highest_num:
+            highest_num = max_val
+
+    latex_plots = """
+    \\begin{figure}[H]
+    \centering
+    """
+    for i in range(0, len(patterns)):
+        graph_data, _, desc = parse_lines(lines, columns, patterns[i])
+        latex_plots += "\\begin{subfigure}"
+        latex_plots += "{0.45\\textwidth}"
+        latex_plots += make_mixed_plot(graph_data, highest_num, desc)
+        latex_plots += """
+            \caption{{{caption}}}
+            \label{{fig:{fig}}}
+            \end{{subfigure}}
+            \hfill
+            """.format(caption=patterns[i].replace('_', " - ").replace("[468]00M", "400M+"),
+                       fig=workload + desc + str(i))
+    tmp = """
+        \caption{{{caption}}}
+        \label{{ fig:{fig} }}
+        \end{{figure}}
+        """
+    if len(columns) == 1:
+        latex_plots += tmp.format(fig=workload + headers[columns[0]].replace('_', ' '),
+                                  caption=headers[columns[0]].replace('_', ' ') + f' for {workload} workload')
+    else:
+        latex_plots += tmp.format(fig="Testing", caption="Testing")
+    # print(latex_plots)
+    return latex_plots
 
 
 def make_three_plot(lines: list[list[str]], columns: list[int]):
@@ -65,7 +111,7 @@ def make_three_plot(lines: list[list[str]], columns: list[int]):
             highest_num = max_val
 
     latex_plots = """
-    \\begin{figure}
+    \\begin{figure}[H]
     \centering
     """
     for i in range(0, len(patterns)):
@@ -150,6 +196,20 @@ def make_unit64_plot(data: str, max_val: int, desc: str):
     return make_plot(data, max_val, desc, datasets, (50, 16), plots)
 
 
+def make_mixed_plot(data: str, max_val: int, desc: str):
+    datasets = """read heavy,
+    equal,
+    write heavy"""
+
+    plots = """
+    \\addplot[draw=black,fill=blue!20, nodes near coords=ALEX] table[x index=0,y index=1] \dataset; %ALEX
+    \\addplot[draw=black,fill=blue!40, nodes near coords=BTree] table[x index=0,y index=2] \dataset; %BTree
+    \\addplot[draw=black,fill=blue!60, nodes near coords=ART] table[x index=0,y index=3] \dataset; %ART
+    \\addplot[draw=black,fill=blue!80, nodes near coords=PGM] table[x index=0,y index=4] \dataset; %PGM
+    """
+    return make_plot(data, max_val, desc, datasets, (50, 12), plots)
+
+
 def make_plot(data: str, max_val: int, desc: str, datasets: str, size: Tuple[int, int], plots: str):
     templ = Template("""
     \pgfplotstableread{
@@ -202,23 +262,29 @@ def make_plot(data: str, max_val: int, desc: str, datasets: str, size: Tuple[int
 # 7 10        68      70  65
 
 def main():
-    workloads = ["read", "write"]
+    workloads = ["read", "write", "mixed"]
     for i in range(0, len(workloads)):
         global workload
         workload = workloads[i]
 
         if workload == "read":
-            lines = read_file("../results_w0_it1.csv")
+            lines = read_file("../w0_avg.csv")
         elif workload == "write":
-            lines = read_file("../results_w100_it1.csv")
+            lines = read_file("../w100_avg.csv")
+        elif workload == "mixed":
+            lines = read_file("../mixed_results.csv")
         global headers
         headers = lines[0]
         del lines[0]
 
         columns = [[4], [8], [11], [12], ]
         for column in columns:
-            with open(f'{workloads[i]}_{headers[column[0]]}.tex'.replace(" ", "_"), 'w') as f:
-                f.write(make_three_plot(lines, column))
+            if workload != "mixed":
+                with open(f'{workloads[i]}_{headers[column[0]]}.tex'.replace(" ", "_"), 'w') as f:
+                    f.write(make_three_plot(lines, column))
+            else:
+                with open(f'{workloads[i]}_{headers[column[0]]}.tex'.replace(" ", "_"), 'w') as f:
+                    f.write(make_mixed_plots(lines, column))
 
 
 if __name__ == "__main__":
